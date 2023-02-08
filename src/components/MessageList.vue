54a610058893fbbe8ca4d6500e0303e20318e099
Cesk <ceskk97@gmail.com>
2023-02-08 19:03:51 +0100

    Modal and MessageList created, New text handler working

---
+++ src/components/MessageList.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import { useMessageStore } from '../stores/message'
+  const { messageList } = useMessageStore()
+</script>
+
+<template>
+  <div v-for="message in messageList" class="">
+    {{ message }}
+  </div>
+</template>
---
+++ src/components/Modal.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import { useMessageStore } from '../stores/message'
+  import MessageList from './MessageList.vue'
+
+  const { addText } = useMessageStore()
+  const text = ref('')
+
+  const handleClick = (e: Event) => {
+    e.preventDefault()
+    addText(text.value)
+  }
+</script>
+
+<template>
+  <section id="modal" class="flex flex-col w-full h-full min-h-[inherit] border-black border-2 rounded-md">
+    <header class="relative border-black border-b-2 p-4">
+      <h1 class="text-2xl font-bold text-center">Chat</h1>
+      <!-- Using blue-600 -->
+      <img src="../assets/close-line-icon.svg" class="absolute right-4 top-2/4 w-6 translate-y-[-50%] cursor-pointer" />
+    </header>
+    <MessageList />
+    <footer class="border-gray-300 border-t-2 p-4">
+      <form class="flex w-full gap-4" @submit="handleClick">
+        <input type="text" v-model="text" name="text" placeholder="Write a message here" class="bg-gray-50 border-gray-300 border-2 rounded-2xl py-1 px-4 w-full" />
+        <button class="flex justify-center items-center cursor-pointer rounded-full bg-blue-500 w-[36px] hover:border-black hover:shadow-sm hover:shadow-cyan-500 active:bg-blue-300">
+          <img src="../assets/send-icon.svg" class="w-5">
+        </button>
+      </form>
+    </footer>
+  </section>
+</template>
